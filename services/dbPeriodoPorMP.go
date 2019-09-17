1bc5f47d023c74db40cb07a31bba6b42c78b7a39
Jorged <jorged@banpais.hn>
2019-09-17 17:00:00 -0600

    Actualización 1659 Horas

--- services/dbPeriodoPorMP.go
+++ services/dbPeriodoPorMP.go
@@ -5,18 +5,6 @@ import (
 	"../models"
 )
 
-func DbPeriodoPorMPAdd(Estructura models.DbPeriodoPorMP) (models.DbPeriodoPorMP, error) {
-
-	Estructura.IdPeriodo = 0
-
-	db := config.ConnectDB()
-	defer db.Close()
-
-	db.Create(&Estructura)
-
-	return Estructura, nil
-}
-
 func DbPeriodoPorMPUPdate(Estructura models.DbPeriodoPorMP) (models.DbPeriodoPorMP, error) {
 
 	var updatedPeriodo models.DbPeriodoPorMP
@@ -26,7 +14,7 @@ func DbPeriodoPorMPUPdate(Estructura models.DbPeriodoPorMP) (models.DbPeriodoPor
 
 	db.Where("idPeriodo = ?", Estructura.IdPeriodo).Find(&updatedPeriodo)
 
-	db.Model(&updatedPeriodo).Where("idPeriodo= ?", Estructura.IdPeriodo).Update(models.DbPeriodoPorMP{IdPeriodo: Estructura.IdPeriodo, IdMP: Estructura.IdMP, IdFrecuencia: Estructura.IdFrecuencia, IdMedicion: Estructura.IdMedicion})
+	db.Model(&updatedPeriodo).Where("idPeriodo= ?", Estructura.IdPeriodo).Update(models.DbPeriodoPorMP{IdFrecuencia: Estructura.IdFrecuencia, IdMedicion: Estructura.IdMedicion})
 
 	return updatedPeriodo, nil
 }
