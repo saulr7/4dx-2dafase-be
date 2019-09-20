fefb6d5138157a75f7be9037a3be6ccedb87ef18
Jorged <jorged@banpais.hn>
2019-09-20 09:29:09 -0600

    Se agregó campo MCI

--- models/ResultadosMCIModels.go
+++ models/ResultadosMCIModels.go
@@ -7,6 +7,7 @@ import (
 type ResultadosMCI struct {
 	IdResultadoMCI    int       `gorm:"column:idResultadoMCI"`
 	IdMCI             int       `gorm:"column:idMCI"`
+	MCI             string       `gorm:"column:MCI"`
 	Anio              int       `gorm:"column:Anio"`
 	Mes               int       `gorm:"column:Mes"`
 	Valor             float32   `gorm:"column:Valor"`
