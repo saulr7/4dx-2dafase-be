2cc1e7eafa98149d8d97382eab7d283aa53e6c64
Jorged <jorged@banpais.hn>
2019-09-19 18:31:58 -0600

    Se agregó los ids

--- models/MedidasPredictivasModels.go
+++ models/MedidasPredictivasModels.go
@@ -9,6 +9,8 @@ type GetMPbyMCI struct {
 	 IdMCI	        	int     `gorm:"column:idMCI"`
 	 IdMP	        	int 	`gorm:"column:idMP"`
 	 IdPeriodo        	int 	`gorm:"column:idPeriodo"`
+	 IdFrecuencia      	int 	`gorm:"column:idFrecuencia"`
+	 IdMedicion        	int 	`gorm:"column:idMedicion"`
 	 MedidaPredictiva   string 	`gorm:"column:MedidaPredictiva"`
 	 MCI            	string 	`gorm:"column:MCI"`	 	
 	 Frecuencia         string  `gorm:"column:Frecuencia"` 	 	 
