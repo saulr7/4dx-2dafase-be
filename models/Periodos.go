992a525b96665f850e8ebe04dcd3d8cb49caefb4
Jorged <jorged@banpais.hn>
2019-09-24 17:34:29 -0600

    Se modificó el campo meta

--- models/Periodos.go
+++ models/Periodos.go
@@ -1,7 +1,7 @@
 package models
 
 import (
-	_ "github.com/jinzhu/gorm"
+//	_"github.com/jinzhu/gorm"
 )
 
 type DbPeriodoPorMP struct {
@@ -19,17 +19,10 @@ func (DbPeriodoPorMP) TableName() string {
 type PeriodoPorMCI struct {
 	// gorm.Model
 	IdPeriodo    int `gorm:"AUTO_INCREMENT;column:idPeriodo"`
-<<<<<<< HEAD
 	IdMCI         int `gorm:"column:idMCI"`
-	IdFrecuencia int `gorm:"column:idFrecuencia"`	
-	Meta float32 `gorm:"column:Meta"`	
-}
-=======
-	IdMCI        int `gorm:"column:idMCI"`
-	IdFrecuencia int `gorm:"column:idFrecuencia"`
+	IdFrecuencia int `gorm:"column:idFrecuencia"`		
 }
 
 func (PeriodoPorMCI) TableName() string {
 	return "dbPeriodoPorMCI"
 }
->>>>>>> 9576035d11bf29e4489fb298b999018294bd25ea
