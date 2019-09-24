97078461f721a6daba7838cda042424a249e1418
Saul Ramirez <saulr@banpais.hn>
2019-09-24 09:46:58 -0600

    Se elimino codigo zombi

--- routes/Routes.go
+++ routes/Routes.go
@@ -9,12 +9,6 @@ func Routes() *mux.Router {
 	myRouter := mux.NewRouter()
 
 	myRouter.HandleFunc("/login", LoginHandler)
-	myRouter.HandleFunc("/home", HomeHandler).Methods("GET")
-	myRouter.HandleFunc("/areas", AreaGetAllHandler).Methods("GET")
-	myRouter.HandleFunc("/area/{key}", AreaOneHandler).Methods("GET")
-	myRouter.HandleFunc("/areaNew", AreaCreateHandler).Methods("POST")
-	myRouter.HandleFunc("/areaUpdate", AreaUpdateHandler).Methods("POST")
-	myRouter.HandleFunc("/areaDelete/{key}", AreaDeleteHandler).Methods("GET")
 	myRouter.HandleFunc("/GetMedidasPredictivas/{codigoEmpleado}", GetMedidasPredictivas).Methods("GET")
 	myRouter.HandleFunc("/GetFrecuencias", GetFrecuencias)
 	myRouter.HandleFunc("/GetMediciones", GetMediciones)
