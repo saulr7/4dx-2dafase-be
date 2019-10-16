7f69f993b97e99e2e02522e4a837c3f857cf76f3
Saul Ramirez <saulr@banpais.hn>
2019-10-15 20:04:52 -0600

    Se agrego el nombre de la tabla la modelo de la brujula

--- models/Brujula.go
+++ models/Brujula.go
@@ -1,9 +1,13 @@
 package models
 
 type Brujula struct {
-	IdBrujula 		int  `gorm:"column:idBrujula"`	
-	IdMP 			int  `gorm:"column:idMP"`	
-	IdColaborador 	int  `gorm:"column:idColaborador"`	
-	Actividad 		string  `gorm:"column:Actividad"`	
-	Estado 			string  `gorm:"column:Estado"`	
+	IdBrujula     int    `gorm:"column:idBrujula"`
+	IdMP          int    `gorm:"column:idMP"`
+	IdColaborador int    `gorm:"column:idColaborador"`
+	Actividad     string `gorm:"column:Actividad"`
+	Estado        string `gorm:"column:Estado"`
+}
+
+func (Brujula) TableName() string {
+	return "dbBrujulaPorMP"
 }
