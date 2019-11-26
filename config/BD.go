e5e0c3be75d0eb785451c7d3364a9bb16a229f55
Saul Ramirez <saulr@banpais.hn>
2019-11-26 09:49:33 -0600

    agregando el archivo config

---
+++ config/BD.go
@@ -0,0 +1,24 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mssql"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+func ConnectDB() *gorm.DB {
+	db, err := gorm.Open("mssql", "sqlserver://app_4dxtablero:app_4dxtablero@des-cobbe01:1433?database=4DX")
+
+	if err != nil {
+		fmt.Println("Algo salió mal")
+		panic(err)
+	}
+
+	// defer db.Close()
+	db.LogMode(true)
+	db.SingularTable(true)
+
+	return db
+}
